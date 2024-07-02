043d1cafb503689aaa3608becf281cee659d9aa9
Walter Nathaniel <wltr.nathaniel@gmail.com>
2024-07-02 22:27:08 +0300

    Added some new files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+
+    const logggedIn = { firstName: "Walter", lastName: "Nathaniel", email: "walternathaniel@proton.me" };
+
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome!"
+                        user={logggedIn?.firstName || "Guest"}
+                        subtext = "Access and manage your account and transactions efficiently."
+                    />
+
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+                RECENT TRANSACTIONS
+            </div>
+
+            <RightSidebar 
+                user={logggedIn}
+                transactions={[]}
+                banks={[]}
+            />
+        </section>
     )
 }
 
